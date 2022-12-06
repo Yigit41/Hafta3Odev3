decd7d6e661bf48fecd43b6d07d554fad6aacfb7
Yiğit Arda YILMAZ <yigit.lisans@outlook.com>
2022-12-06 12:49:20 +0300

    Hafta3Odev3

---
+++ Hafta3Odev3/src/patikadev/InvertedTriangel.java
@@ -0,0 +1,24 @@
+package patikadev;
+
+import java.util.Scanner;
+
+public class InvertedTriangel {
+
+	public static void main(String[] args) {
+		 	Scanner input = new Scanner(System.in);
+	        System.out.print("Please enter a number : ");
+	        int n = input.nextInt();
+
+	        for (int i=n; i>=1; i--){
+	            for (int k=n + 1; k>i; k--){
+	                System.out.print(" ");
+	            }
+	            for (int l=1; l<=(2*i-1); l++){
+	                System.out.print("*");
+	            }
+	            System.out.println();
+
+	        }
+	}
+
+}
